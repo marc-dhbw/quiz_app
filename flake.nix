3f35b650d7ddf31c5f07d2cf31e6417bd085cf65
marc55s <marc.schoenig@gmail.com>
2025-01-15 12:55:46 +0100

    added flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";  # Use the latest stable version or your desired branch
+    };
+
+    outputs = { self, nixpkgs }: 
+        let
+            pkgs = import nixpkgs { inherit system; };
+            system = "x86_64-linux";
+        in{
+
+            devShell.${system} = pkgs.mkShell {
+                buildInputs = [ pkgs.docker-compose ];
+                shellHook = ''
+                    docker-compose up -d
+                '';
+            };
+        };
+
+}
